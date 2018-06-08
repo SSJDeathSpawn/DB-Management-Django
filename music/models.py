a60a77b245e7e40bbabf415a64889edd297b3dc2
SSJDeathSpawn <adikesavan28@gmail.com>
2018-06-08 19:32:21 +0530

    Implementation of models

--- music/models.py
+++ music/models.py
@@ -7,5 +7,8 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
 
+
 class Song(models.Model):
-    album = models.ForeignKey(Album, on_delete=models.CASCADE())
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=250)
